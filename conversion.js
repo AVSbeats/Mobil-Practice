ac450c5ad7f65ce9e0266fae0d603d1cee446eaa
Abraham Vallejos-Soto <125098951+AVSbeats@users.noreply.github.com>
2023-03-22 13:49:17 -0400

    First to commit

---
+++ conversion.js
@@ -0,0 +1,6 @@
+function calculateConversion () {
+    var input = document.getElementById("gallons").value;
+
+    var inputElement = document.getElementById("result");
+    inputElement.innerHTML = input;
+}
